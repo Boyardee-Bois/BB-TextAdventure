d2828e21947edb477d92d453ecbe1853a4334fd3
j-klenk <77705398+j-klenk@users.noreply.github.com>
2025-10-12 20:04:11 -0400

    BB-23 Added Player Obj to Main
    Update the player class to follow class structure and naming conventions.
    
    Added a player object to main to test if player can be placed in a room.

--- BB-TextAdventure/BB-TextAdventure.cpp
+++ BB-TextAdventure/BB-TextAdventure.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "UI.h"
+#include "Player.h"
 using namespace std; 
 
 
@@ -8,4 +9,14 @@ int main()
 	UI::GameIntro();
 	UI::HelpMenu();
 
+	Room startRoom("Lab", "You find yourself in a dimly lit lab with scattered papers.");
+	Room hallwayRoom("Hallway", "You see a long hallway leading to the outdoors.");
+
+	startRoom.addExit("North", &hallwayRoom);
+
+	Player player(&startRoom);
+
+	cout << "You being in:" << player.getCurrentRoom()->getRoomName() << endl;
+	
+	player.Movement_Counter();
 }
