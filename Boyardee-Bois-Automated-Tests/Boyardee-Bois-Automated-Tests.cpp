dfdfebe7c624e4291f16c00ba00e1defba8423dc
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-10-27 12:26:53 -0400

    BB-55 - created & ran Hello_BB_AutoTest case

---
+++ Boyardee-Bois-Automated-Tests/Boyardee-Bois-Automated-Tests.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../BB-TextAdventure/Map.cpp"
+#include "../BB-TextAdventure/Tile.cpp"
+#include "../BB-TextAdventure/UI.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BoyardeeBoisAutomatedTests
+{
+	TEST_CLASS(BoyardeeBoisAutomatedTests)
+	{
+	public:
+		
+		TEST_METHOD(Hello_BB_AutoTest)
+		{
+			Logger::WriteMessage("Hello from Boyardee Bois AutomatedTestProject\n");
+		}
+	};
+}
