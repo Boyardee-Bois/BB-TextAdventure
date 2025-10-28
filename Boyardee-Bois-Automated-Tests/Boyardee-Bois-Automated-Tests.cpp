18dcd5d9581fd2e2446bdf922a9f4c1809bf62ba
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-10-27 20:48:42 -0400

    BB-55 - added validation test case, at least for first two rows of 10.  For demo purposes intentionally failed comparison of tile[1][9]

--- Boyardee-Bois-Automated-Tests/Boyardee-Bois-Automated-Tests.cpp
+++ Boyardee-Bois-Automated-Tests/Boyardee-Bois-Automated-Tests.cpp
@@ -12,10 +12,16 @@ namespace BoyardeeBoisAutomatedTests
 	{
 	public:
 		
+		// test method to demonstrate the test project is correctly linked and can be run from test explorer
 		TEST_METHOD(Hello_BB_AutoTest)
 		{
 			Logger::WriteMessage("Hello from Boyardee Bois AutomatedTestProject\n");
 		}
+
+		// Get_Grid test case reads the specified section of the grid and prints it to the test output
+		//    it doesn't do any validation of expected results
+		//    the MAX_ROWS and MAX_COLUMNS is configurable here (actual in game is 25x25)
+		//      ideally should add methods to the Map class to return the actual grid size & use those
 		TEST_METHOD(Get_Grid) 
 		{
 			const int MAX_ROWS = 10;   // for now just pull first 10 rows & columns, rather than all 25
@@ -34,6 +40,41 @@ namespace BoyardeeBoisAutomatedTests
 				outputString += "row" + to_string(rowIterator) + "\n";
 				Logger::WriteMessage(outputString.c_str());
 			}
+		}
+		// Validate_Grid - reads the grid from the map & validates against an expected grid
+		//   for example - tile [0,0] is grass, ....
+		//   can move much of the duplicate test code to a helper function, but 
+		TEST_METHOD(Validate_Grid) {
+			const int MAX_ROWS = 2;   // for now just pull first 10 rows & columns, rather than all 25
+			const int MAX_COLUMNS = 10;
+			Map gridMap;              // initializes the grid
+			Tile returnedTile;
+			char gridCharArray[MAX_ROWS][MAX_COLUMNS];         // for downloading the initialized grid from the Map class
+			char expectedGridCharArray[MAX_ROWS][MAX_COLUMNS] = {     // this is the expected grid, with tiles aliased to characters - e.g. Grass = 'G'
+				{'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G', 'G'},
+				{'G', 'G', 'G', 'S', 'S', 'S', 'S', 'G', 'G', 'W'} };
+
+			// loop through the Map grid - for each row, retrieve all of the column tiles
+			//   store the received grid (no specific reason to do so, but ...)
+			//   compare each tile to the expected tile, if not equal, fail the test case (ie Assert)
+			// prints each successfully compared row on the test output
+			//    Note: for demonstration purposes switched cell[1][9] from Grass to Water to intentionallly fail the test case
+			for (int rowIterator = 0; rowIterator < MAX_ROWS; rowIterator++) {
+				string outputString = "";
+				for (int columnIterator = 0; columnIterator < MAX_COLUMNS; columnIterator++) {
+					// get the tile, store it and add to print string
+					returnedTile = gridMap.returnTile(rowIterator, columnIterator);
+					gridCharArray[rowIterator][columnIterator] = returnedTile.mapTileToChar();
+					outputString = outputString + gridCharArray[rowIterator][columnIterator] + " ";
+
+					// verify the tile matches the expected tile
+					Assert::AreEqual(expectedGridCharArray[rowIterator][columnIterator],
+						             gridCharArray[rowIterator][columnIterator],
+						L"Tile did not match!");
+				}  // end of row
+				outputString += "row" + to_string(rowIterator) + "\n";
+				Logger::WriteMessage(outputString.c_str());
+			}  // end of all rows
 		}
 	};
 }
