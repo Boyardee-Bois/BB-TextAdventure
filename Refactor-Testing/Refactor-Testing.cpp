acec1d66437826507718c6a6ceb7218d6545dcd4
BrooksideCoding <dbrooksbank@student.fdu.edu>
2025-11-08 17:32:20 -0500

    Started working on the automated testing

---
+++ Refactor-Testing/Refactor-Testing.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <iostream>
+#include "../BB-TextAdventure/World.h"
+#include "../BB-TextAdventure/Zone.h"
+#include "../BB-TextAdventure/Player.h"
+#include "../BB-TextAdventure/Renderer.h"
+#include "../BB-TextAdventure/CommandParser.h"
+#include "../BB-TextAdventure/UI.h"
+#include "../BB-TextAdventure/PlayerActionManager.h"
+#include "../BB-TextAdventure/Tile.h"
+#include "../BB-TextAdventure/HealthUI.h"
+#include <map>
+#include <string>
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RefactorTesting
+{
+	TEST_CLASS(RefactorTesting)
+	{
+	public:
+		
+		TEST_METHOD(MapRenderer)
+		{
+			Logger::WriteMessage("Test if the Map renders properly");
+
+		}
+	};
+}
